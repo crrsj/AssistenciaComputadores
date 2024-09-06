93e453578f5b137ba8fc400bf371f9c59b9fe4c9
Carlos Roberto <crrsj1@gmail.com>
2024-09-06 18:13:29 -0300

    Criando metodos buscarPorId,BuscarTodos,atualizarCliente,excluir,na classe ClienteService

--- src/main/java/br/com/infotech/enums/Status.java
+++ src/main/java/br/com/infotech/enums/Status.java
@@ -1,5 +1,6 @@
 package br.com.infotech.enums;
 
 public enum Status {
-ENTREGUE,CONSERTANDO;
+ENTREGUE,
+CONSERTANDO;
 }
--- src/main/java/br/com/infotech/servico/ClienteServico.java
+++ src/main/java/br/com/infotech/servico/ClienteServico.java
@@ -1,5 +1,8 @@
 package br.com.infotech.servico;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import br.com.infotech.dto.ClienteDto;
@@ -19,6 +22,23 @@ public class ClienteServico {
 		
 	}
 	
-	
+	public List<ClienteDto>buscarTodos(){
+		var lista = clienteRepositorio.findAll().
+		stream().map(ClienteDto::new).toList();
+		return lista;
+	}
 
+	public Cliente buscarPorId(Long id) {
+		Optional<Cliente>buscar = clienteRepositorio.findById(id);
+		return buscar.get();
+	}
+	
+	public Cliente atualizarCliente(ClienteDto cliente) {		
+		var atualizar = new Cliente(cliente);
+		return clienteRepositorio.save(atualizar);
+		
+	}
+	public void excluir(Long id) {
+		clienteRepositorio.deleteById(id);
+	}
 }
